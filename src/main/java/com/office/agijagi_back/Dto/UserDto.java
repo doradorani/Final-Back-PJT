e80bfad1e445fb321036b6199c536e108f5afe59
jangdebug <jangdebug@naver.com>
2023-10-31 12:12:57 +0900

    OF-51-UserDetails-UserDetails로-인한-사용자-정보-권한-문제-처리
    ## UserDto에 implement UserDetail을 했을 때
    
    <aside>
    💡 UserDetail에서 제공하는 권한 관련 하여 UserDto에서 정보를 가져오려고 할 때
    Collection<? extends GrantedAuthority> getAuthorities() 에서
    시큐리티에서 무조건 getAuthorities의 NullException을 확인하여
    프론트에서 500번에러가 발생하는 문제가 생김
    
    </aside>
    
    해결)
    
    사용자 정보를 저장하는 UserDto와 JWT 사용자 권한 관련 AuthDto로 나누고
    
    AuthDto에 implement UserDetail을 적용

--- src/main/java/com/office/agijagi_back/Dto/UserDto.java
+++ src/main/java/com/office/agijagi_back/Dto/UserDto.java
@@ -2,60 +2,13 @@ package com.office.agijagi_back.Dto;
 
 import com.github.rkpunjal.sqlsafe.SQLInjectionSafe;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
-@Builder
 @AllArgsConstructor
-public class UserDto implements UserDetails {
+public class UserDto {
 
     private @SQLInjectionSafe String id;
     private @SQLInjectionSafe String email;
-    private @SQLInjectionSafe List<String> roles;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.roles.stream()
-                .map(SimpleGrantedAuthority::new)
-                .collect(   Collectors.toList());
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
 }
-
