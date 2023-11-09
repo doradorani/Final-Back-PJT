4649344e99431168319aa7eabd5181f9a5cc78d2
jangdebug <jangdebug@naver.com>
2023-11-09 18:55:01 +0900

    사용자 펀딩 좋아요 기능 구현  + 사용자 회원가입 시, 수정창으로 이동

--- src/main/java/com/office/agijagi_back/Mapper/ICobuyingMapper.java
+++ src/main/java/com/office/agijagi_back/Mapper/ICobuyingMapper.java
@@ -14,4 +14,20 @@ public interface ICobuyingMapper {
     int productTotalPage(int perPage);
     CoBuyProductDto detailProductNo(int detailProductNo);
     int accumulateProduct(int detailProductNo);
+    int fundingProduct(String email, int detailProductNo, String selectedOption);
+    int alreadyFunding(String email, int detailProductNo);
+    List<Integer> myFundings(String email);
+    List<Integer> myHits(String email);
+    int alreadyHit(String email, int detailProductNo);
+    int cobuyInsertHit(String email, int detailProductNo);
+    int cobuyDeleteHit(String email, int detailProductNo);
+    int decreaseHit(int detailProductNo);
+    int increaseHit(int detailProductNo);
+    int getCobuyHitByNo(int detailProductNo);
+    List<CoBuyProductDto> myFundingProduct(String email, int perPage, int offset);
+    int fundingListCnt(String email);
+    int fundingTotalPage(String email, int perPage);
+    List<CoBuyProductDto> myHitProduct(String email, int perPage, int offset);
+    int hitListCnt(String email);
+    int hitTotalPage(String email, int perPage);
 }
--- src/main/java/com/office/agijagi_back/Service/Interface/ICobuyingService.java
+++ src/main/java/com/office/agijagi_back/Service/Interface/ICobuyingService.java
@@ -11,4 +11,9 @@ public interface ICobuyingService {
     public int coBuyListCnt();
     public Map<String, Object> coBuyList(int currentPage, int perPage);
     public Map<String, Object> detailProductNo(int detailProductNo);
+    public int fundingProduct(String email, int detailProductNo, String selectedOption);
+    public Map<String, Object> myCobuy(String email);
+    public int cobuyHit(String email, int detailProductNo);
+    public Map<String, Object> myFundingProduct(String email, int currentPage, int perPage);
+    public Map<String, Object> myHitProduct(String email, int currentPage, int perPage);
 }
